6e0be048012b849f9e0ccc2246d5da1d9c54e89a
Olga Python <olga_python@proton.me>
2025-08-20 00:42:28 +0200

    update files

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from catalog.models import Product
+
+
+# def home(request):
+#     return render(request, 'home.html')
+
 
 def home(request):
-    return render(request, 'home.html')
+    # Получаем 5 последних продуктов по дате создания
+    latest_products = Product.objects.all().order_by('-created_at')[:5]
+
+    # Выводим каждый продукт в консоль (для отладки)
+    for product in latest_products:
+        print(product)
+
+    return render(request, 'home.html', {'latest_products': latest_products})
 
 
 def contacts(request):
--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+
+from django.conf.global_settings import MEDIA_URL, MEDIA_ROOT
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv(override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9^%x#-^8@n#xm#*1-%g@nu9@5gi@291+@vo738fttx*x4e!el0"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv('DEBUG') == 'True' else False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -75,8 +81,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
 
@@ -115,9 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = (BASE_DIR / 'static',)
+STATICFILES_DIRS = [BASE_DIR / 'static']
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
