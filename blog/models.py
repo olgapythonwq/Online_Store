83ca8e0dcb0392e6f2564db90a09a8daed401022
Olga Python <olga_python@proton.me>
2025-09-07 17:53:13 +0200

    update blog files

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Title")
+    content = models.TextField(verbose_name="Content")
+    preview = models.ImageField(upload_to='blog_previews/', blank=True, null=True, verbose_name="Preview")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Creation date")
+    is_published = models.BooleanField(default=False, verbose_name="Published")
+    views_count = models.PositiveIntegerField(default=0, verbose_name="Views")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'пост'
+        verbose_name_plural = 'посты'
+        ordering = ['title',]
---
+++ blog/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
+from .models import Post
+from .forms import PostForm
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Post.objects.filter(is_published=True).order_by('-created_at')
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'  # Задаём имя переменной, под которым объект поста будет доступен в шаблоне.
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)  # Переопределяем метод, чтобы увеличивать количество просмотров.
+        obj.views_count += 1
+        obj.save(update_fields=['views_count'])  # Сохраняем объект, но только поле views_count.- Это оптимизированный способ, чтобы не обновлять все поля в базе, а только одно.
+        return obj
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('blog:post_list')
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.is_published = True  # Автоматически публикуем пост
+        post.save()
+        return super().form_valid(form)
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('blog:post_list')
+
+    def get_success_url(self):
+        return reverse('blog:post_detail', kwargs={'pk': self.object.pk})
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('blog:post_list')
