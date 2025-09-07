c406d260d14fe0c360adae7faa69a4c51312d16a
Olga Python <olga_python@proton.me>
2025-09-07 17:50:42 +0200

    update context_processors.py, catalog/urls.py and catalog/views.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, product_info, categories_list
+from catalog.views import ProductListView, ProductDetailView, ProductsByCategoryView, ContactsView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('home/', home, name='home'),
-    path('base/', categories_list, name='categories_list'),
-    path('contacts/', contacts, name='contacts'),
-    path('product_info/<int:product_id>/', product_info, name='product_info'),
+    path('', ProductListView.as_view(), name='catalog'),  # Главная страница
+    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),  # Детали товара
+    path('category/<int:pk>/', ProductsByCategoryView.as_view(), name='products_by_category'),  # Товары по категории
 ]
 
