80ea89b7173eaf04ca497f62abaa6b763364b031
Olga Python <olga_python@proton.me>
2025-09-13 21:26:34 +0200

    update context_processors.py

--- catalog/context_processors.py
+++ catalog/context_processors.py
@@ -2,4 +2,4 @@ from .models import Category
 
 def categories_processor(request):
     categories = Category.objects.all()
-    return {'categories_list': categories}
+    return {'categories': categories}
