2e560564ee9eb5e5e74951fa5ba17a2f2fb5ad59
Olga Python <olga_python@proton.me>
2025-09-18 23:16:09 +0200

    update config files ans blog/views.py

--- config/urls.py
+++ config/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
     path('blog/', include('blog.urls', namespace='blog')),  # подключаем blog
+    path('users/', include('users.urls', namespace='users')),
 ]
 
 if settings.DEBUG:
